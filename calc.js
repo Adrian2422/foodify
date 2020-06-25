abd4910724fde90353f92026c1d05fa92d7828a8
Programista <adrian.kloc47@gmail.com>
2020-06-25 20:07:19 +0200

    added personal survey for daily kcal calculation, minor js, css and html improvements

--- calc.js
+++ calc.js
@@ -5,6 +5,7 @@ const surveyAmount = document.querySelector('.surveyColMid input');
 const resultDiv = document.querySelector('.result');
 const resultBtn = document.querySelector('.showResult');
 const itemList = document.querySelector('.itemList');
+const personalBtn = document.querySelector('.personalBtn');
 
 //flaga ilości itemów
 let itemCount = 0;
@@ -169,7 +170,43 @@ const sumKcal = (nodes) => {
         }
         sum += obj['amount'] * (kcal(products, `${obj['name']}`))/100;
     })
-    return sum.toFixed(2);
+    return sum.toFixed();
+}
+const calcPersonalKcal = () => {
+    const gender = document.querySelector('input[name=gender]:checked').id;
+    const age = document.querySelector('#age').value;
+    const height = document.querySelector('#height').value;
+    const weight = document.querySelector('#weight').value;
+    const activitySelect = document.querySelector('#activity');
+    let activity = activitySelect.options[activitySelect.selectedIndex].value;
+    const activityTab = [1, 1.10, 1.15, 1.2];
+    if(gender === 'man'){
+        return ((9.99 * weight) + (6.25 * height) - (4.92 * age) + 5) * activityTab[activity];
+    } else {
+        return ((9.99 * weight) + (6.25 * height) - (4.92 * age) - 161) * activityTab[activity];
+    }
+}
+const fancyDisplay = (num, selector, str) => {
+    const par = document.querySelector(selector);
+    par.innerText = "0";
+    let actual = 0;
+
+    const time = setInterval(() =>{
+        if(num > 1000){
+            actual += 50;
+        } else if (num > 100){
+            actual += 10;
+        } else {
+            actual++;
+        }
+        par.innerText = `${str}${actual}`;
+
+        if(actual >= num){
+            clearInterval(time);
+            par.innerText = `${str}${num}`;
+            createVerdictHtml();
+        }
+    }, 10);
 }
 const getNodesValues = () => {
     const divs = document.querySelectorAll('.itemDiv');
@@ -189,24 +226,34 @@ const createResultHtml = () => {
     resultBtn.remove();
     const kcal_amount = sumKcal(getNodesValues());
     const dispKcal = document.createElement('div');
-    const kcal = document.createElement('p');
-    //const dispNutrients = document.createElement('div');
-    //const nutruents = document.createElement('ul');
-    //const nutrientsItem = document.createElement('li');
+    const productsSetKcal = document.createElement('p');
     const resetBtn = document.createElement('button');
     resetBtn.classList.add('resetBtn');
     resetBtn.setAttribute('id', 'resetBtn');
     dispKcal.classList.add('dispKcal');
-    //dispNutrients.classList.add('dispNutrients');
-    kcal.innerText = "Ilość kcal w zestawie: " + kcal_amount;
-    //nutrientsItem.innerText = "Tłuszcze: ";
+    productsSetKcal.classList.add('productsSetKcal');
     resetBtn.innerText = "Oblicz jeszcze raz";
-    dispKcal.appendChild(kcal);
-    //nutruents.appendChild(nutrientsItem);
-    //dispNutrients.appendChild(nutruents);
+    dispKcal.appendChild(productsSetKcal);
     resultDiv.appendChild(dispKcal);
-    // resultDiv.appendChild(dispNutrients);
     resultDiv.appendChild(resetBtn);
+    fancyDisplay(kcal_amount, '.dispKcal p', 'Ilość kcal w zestawie: ');
+}
+const createVerdictHtml = () => {
+    if(document.body.contains(document.querySelector('.dispKcal'))) {
+        const dispKcal = document.querySelector('.dispKcal');
+        const verdict = document.createElement('p');
+        verdict.classList.add('verdict');
+        dispKcal.appendChild(verdict);
+        const personalKcal = document.querySelector('.personalKcal').innerText;
+        const productsSetKcal = document.querySelector('.productsSetKcal').innerText;
+        const kcalRatio = ((productsSetKcal.split(': ')[1]) / (personalKcal.split(': ')[1]) * 100).toFixed();
+        const kcalDiff = ((personalKcal.split(': ')[1]) - (productsSetKcal.split(': ')[1])).toFixed();
+        if(kcalRatio < 95){
+            verdict.innerText = `Dzienne spożycie mniejsze o ${kcalDiff} od zalecanego`;
+        } else if(kcalRatio > 100){
+            verdict.innerText = `Dzienne spożycie większe o ${Math.abs(kcalDiff)} od zalecanego`;
+        }
+    }
 }
 
 //klasa dla kolejnych itemów z listy
@@ -265,6 +312,29 @@ surveyAccept.addEventListener('click', e => {
 surveyAmount.addEventListener('input', e => {
     onlyNumInput(surveyAmount);
     survey._amount = Number(surveyAmount.value);
+})
+personalBtn.addEventListener('click', e =>{
+    if(document.querySelector('#age').value === ""){
+        alert('Proszę podać wiek');
+    } else if (document.querySelector('#height').value === ""){
+        alert('Proszę podać wzrost');
+    } else if(document.querySelector('#weight').value === ""){
+        alert('Proszę podać wagę');
+    } else if(document.querySelector('#activity').options[document.querySelector('#activity').selectedIndex].value === "0"){
+        alert('Proszę wybrać rodzaj aktywności');
+    } else {
+        if(document.body.contains(document.querySelector('.personalDiv'))){
+            const personalDiv = document.querySelector('.personalDiv');
+            personalDiv.remove();
+        }
+        const personalDiv = document.createElement('div');
+        const personalKcal = document.createElement('p');
+        personalKcal.classList.add('personalKcal');
+        personalDiv.classList.add('personalDiv');
+        personalDiv.appendChild(personalKcal);
+        resultDiv.prepend(personalDiv);
+        fancyDisplay(calcPersonalKcal().toFixed(), '.personalKcal', 'Twoje zapotrzebowanie na kalorie wynosi: ');
+    }
 })
 resultBtn.addEventListener('click', e => {
     if(!document.body.contains(document.querySelector('.itemDiv'))){
